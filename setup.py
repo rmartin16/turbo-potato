6da1f4ce41bf7b016f49afb0fa09ccfb1c3b4bd2
russell <rmartin16@gmail.com>
2020-05-20 22:57:40 -0400

    add setup.py so proper installation works

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='turbo-potato',
+    version='0.0.0',
+    packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests', 'scripts']),
+    include_package_data=True,
+    install_requires=['unidecode',
+                      'PyInquirer',
+                      'git+git://github.com/divijbindlish/parse-torrent-name.git@PTN',
+                      'requests',
+                      'tvdbsimple',
+                      'tmdbsimple',
+                      'urwid',
+                      'qbittorrent-api',
+                      'nltk'],
+    url='https://github.com/rmartin16/turbo-potato',
+    author='Russell Martin',
+    description='Media torrent manager',
+    zip_safe=False,
+    license='MIT',
+    classifiers=['Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.6']
+)
